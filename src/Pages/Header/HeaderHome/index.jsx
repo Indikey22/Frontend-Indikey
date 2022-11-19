0a22b403f0353f6ecd8f0f723627f68b7191eeaf
Gugzera <gustavo.gomes.lhp@hotmail.com>
2022-11-19 15:12:49 -0300

    header principal feita

--- src/Pages/Header/HeaderHome/index.jsx
+++ src/Pages/Header/HeaderHome/index.jsx
@@ -1,27 +1,54 @@
 import { Link } from 'react-router-dom';
-import Home from '../../Home/HomePersona/index';
-import './styles.css';
+import Home from '../../Home/HomePersona';
 
-function Header(){
-    return(
-        <>
-        <header>
-            <div className='logo'>
-                <img src="" alt="logo" />
+
+import './styles.css'
+import logo from '../../../assets/img/logo.svg'
+
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+                
+function Header() {
+  return (
+    <>  
+    <div className="MenuTopo">
+    <Link to='/'>
+        <Fade top>  
+        <img src={logo} alt="logo indikey"  className="logoTopo" />
+        </Fade>
+    </Link>
+        <Navbar id="topo"  expand="lg">
+        <Container id="containerTopo">
+            <Navbar.Toggle id="mobile-menu"  aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="listaTopo"  id="basic-navbar-nav">
+            <Nav id="listaTopo" className="me-auto">
+                <Nav.Link className="text-body"  href="#home"> App Mobile</Nav.Link>
+                <NavDropdown className="sobreNos"  title="Sobre Nós" id="basic-nav-dropdown">
+                <NavDropdown.Item className="text-body" href="#action/3.1">Palavra</NavDropdown.Item>
+                <NavDropdown.Item className="text-body" href="#action/3.2">Propósito</NavDropdown.Item>                
+                <NavDropdown.Item className="text-body" href="#action/3.2">indicações</NavDropdown.Item>                
+                </NavDropdown>
+                <Nav.Link className="text-body" href="#home">Contato</Nav.Link>
+            </Nav>
+
+            <div className="BotoesTopo">
+              <button className="btnCadastrarTopo">Cadastrar</button>
+              <button className="btnLoginTopo">Login</button>
             </div>
-            <nav>
-            <Link to='/'><li>Como funciona</li></Link>
-            <Link to='/'><li>Sobre nós</li></Link>
-            <Link to='/'><li>E-Comerce</li></Link>
-            </nav>
-            <div className="botoes">
-            <Link to='/'><button>Entrar</button></Link>
-            <Link to='/'><button>Cadastrar</button></Link>
-            </div>
-        </header>
+            </Navbar.Collapse>
+ 
+        </Container>
+        </Navbar>
+        </div>
         <Home />
         </>
-    );
+  );
 }
 
 export default Header;
