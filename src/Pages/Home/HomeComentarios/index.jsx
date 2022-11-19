19feaa626f8a254e8abb3944343718a67949f31d
Pedro Neto <netopedroo117@gmail.com>
2022-11-19 17:55:32 -0300

    Slides comentarios

--- src/Pages/Home/HomeComentarios/index.jsx
+++ src/Pages/Home/HomeComentarios/index.jsx
@@ -1,16 +1,47 @@
 import './styles.css';
-import imgFundoComentarios from '../../../assets/img/fundoComentarios.svg'
+import SetaEsquerda from '../../../assets/img/arrowleft.svg'
+import SetaDireita from '../../../assets/img/arrowright.svg'
+import Comentario from '../../../assets/img/comentario.svg'
 
 function Comentarios() {
+    var intervalo = 0;
+
+    var maxSlider = document.querySelectorAll('card').length - 1;
+    acao();
+    function acao() {
+        let img = document.querySelectorAll('.card img')
+
+        setInterval(function () {
+            img[intervalo].getElementsByClassName.display = 'none';
+            intervalo++;
+            if (intervalo > maxSlider) {
+
+                intervalo = 0;
+            }
+            img[intervalo].getElementsByClassName.display = 'block';
+        }, 3000)
+    }
     return (
-        <>
-            <div className='todo'>
-
-                <img src={imgFundoComentarios} alt='img-back' />
-
+        <article className='comentarios'>
+            <div className='botoes'>
+                <button className='arrow-left'><img src={SetaEsquerda} alt="seta esquerda" /></button>
+                <button className='arrow-right'><img src={SetaDireita} alt="seta direita" /></button>
             </div>
-        </>
+            <div className='slides'>
+                <div className='box'>
+                    <img src={Comentario} alt=" foto de perfil" />
+                </div>
+                <div className='box'>
+                    <img src={Comentario} alt=" foto de perfil" />
+                </div>
+                <div className='box'>
+                    <img src={Comentario} alt=" foto de perfil" />
+                </div>
+            </div>
+        </article>
     );
 }
 
 export default Comentarios;
+
+
