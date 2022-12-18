b17016aa345db6940e5a84ebf6a24b2e31561c1b
Gugz3ra <gustavo.gomes.lhp@hotmail.com>
2022-12-17 21:57:43 -0300

    fazendo pg contato com o autonomo

--- src/Pages/Vendas/Contratos/index.jsx
+++ src/Pages/Vendas/Contratos/index.jsx
@@ -1,31 +1,38 @@
 import './styles.css';
 import HeaderEcommerce from "../../../components/Headers/HeaderEcommerce/index";
+import FotodePerfil from '../../../assets/img/perfilusuario.svg';
 
 function Contratos(){
     return(
         <>
         <HeaderEcommerce />
         <main className='contratos'>
-            <div>
-                <img src="" alt="" />
-                <h2>Antonio Superbi</h2>
-                <h5>Organizador de Eventos</h5>
+            <section id="autonomoTopo">
+                <div className="infoPerfil">
+                    <img src={FotodePerfil} alt="foto do perfil do autônomo" id="imgPerfil"/>
+                    <div>
+                        <h2>Antonio Superbi</h2>
+                        <h5>Organizador de Eventos</h5>
+                    </div>
+                </div>
+            </section>
+            <div className='informativo'>
+                <h2>Entrar em contato</h2>
+                <section id='informacoesAutonomo'>
+                    <div className="descricaoautonomo">
+                        <textarea name="dados" id="dadosAutonomo" cols="40" rows="14"></textarea>
+                    </div>
+                    <form action="" id='formAutonomo'>
+                        <label htmlFor="">Nome</label>
+                        <input type="text" />
+                        <label htmlFor="">Email</label>
+                        <input type="text" />
+                        <label htmlFor="">Mensagem</label>
+                        <input id='msgAutonomo' ></input>
+                        <button id="btnEnviarAutonomo">Enviar</button>
+                    </form>
+                </section>
             </div>
-        <div className='informativo'>
-            <h3>Entrar em contato</h3>
-            <div className="descricaoautonomo">
-                oi
-            </div>
-            <form action="">
-                <label htmlFor="">Nome</label>
-                <input type="text" />
-                <label htmlFor="">Email</label>
-                <input type="text" />
-                <label htmlFor="">Mensagem</label>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
-                <button>Enviar</button>
-            </form>
-        </div>
         </main>
         </>
     );
